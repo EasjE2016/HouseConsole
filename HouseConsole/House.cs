57777eb2f30d8fef3b0030e1b61ecac30953d8bc
EASJ <EASJ@EASJ3212>
2016-09-22 16:32:47 +0200

    Add project files.

---
+++ HouseConsole/House.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseConsole
+{
+    public class House
+    {
+        private string adresse;  //field
+
+        public string Navn { get; set; } //property
+        public int HusNr { get; set; } //property
+
+        public House() //default constructor
+        {
+
+        }
+
+        public House(string adresse)  //konstructor med en parameter som har typen string
+        {
+            this.adresse = adresse;
+        }
+
+
+        public string GetAdresse() //metode som returnerer adresse
+        {
+            return this.adresse;
+        }
+
+        public void SetAdresse(string adresse) //metode hvor adress sættes, da metoden ikke returnerer noget har den typen void
+        {
+            this.adresse = adresse;
+        }
+
+    }
+}
---
+++ HouseConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House Hus1 = new House(); // laver et objekt/instans af House som jeg navngiver Hus1 , Jeg kalder default constructor
+
+           
+
+
+            House Hus2 = new House("Solvej"); //laver et objekt/instans af House som jeg navngiver Hus2, Jeg kalder construktoren som tager en string parameter
+
+            Hus2.HusNr = 27; // sætter husnr på Hus2 til værdien 27
+            Hus2.Navn = "Murstenshus"; //sætter Navn på Hus2 til værdien "Murstenshus"
+
+            Console.WriteLine("Adresse: {0} ",Hus2.GetAdresse()); // Kalder metoden GetAdresse() på objektet/instansen Hus2 og printer resultatet ud til konsollen
+
+        }
+    }
+}
